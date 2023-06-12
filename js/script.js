5b78e9b0eea47a8b45e98de367460f9fb6589bc1
Develop <110947951+camposrdi@users.noreply.github.com>
2023-06-11 19:44:57 -0600

    Update form for send emails

--- js/script.js
+++ js/script.js
@@ -64,3 +64,80 @@ window.onscroll = () => {
     loop: true
    })
 
+
+   // Llamar a la función 'authorize' al cargar la página
+window.onload = function() {
+  handleClientLoad();
+};
+
+// Cargar la biblioteca de la API de Gmail
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+// Inicializar la API de Gmail
+function initClient() {
+  gapi.client
+    .init({
+      apiKey: "AIzaSyBtH4v8KZv9TiqfjdIjNm3ns2wHvAj29Qg",
+      clientId: "Tenvio-de-correo-389600",
+      discoveryDocs: [
+        "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
+      ],
+      scope: "https://www.googleapis.com/auth/gmail.send",
+    })
+    .then(
+      function () {
+        // Autenticación exitosa
+        gapi.auth2.getAuthInstance().signIn();
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+}
+
+// Enviar el correo electrónico
+function sendEmail() {
+  var form = document.getElementById("emailForm");
+  var namef = form.elements.namef.value;
+  var email = form.elements.email.value;
+  var phone = form.elements.phone.value;
+  var subject = form.elements.subject.value;
+  var message = form.elements.message.value;
+
+  var email = [
+    'Content-Type: text/plain; charset="UTF-8"\r\n',
+    "MIME-Version: 1.0\r\n",
+    "From: " + email + "\r\n",
+    "To: " + namef + "\r\n",
+    "Subject: " + subject + "\r\n\r\n",
+    message,
+  ].join("");
+
+  var base64EncodedEmail = btoa(email).replace(/\+/g, "-").replace(/\//g, "_");
+
+  gapi.client.gmail.users.messages
+    .send({
+      userId: "me",
+      resource: {
+        raw: base64EncodedEmail,
+      },
+    })
+    .then(
+      function (response) {
+        console.log("Correo electrónico enviado.");
+        form.reset();
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+
+  return false;
+}
+
+
+// Agregar el evento submit al formulario
+var form = document.getElementById('emailForm');
+form.addEventListener('submit', sendEmail);
